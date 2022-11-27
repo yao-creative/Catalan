2cc7e7921c7b43602899bc6e8a1749a5709a44e2
Yi Yao Tan <yytanwork@gmail.com>
2022-11-27 17:41:15 +0100

    contour simulations added

---
+++ sampling.py
@@ -0,0 +1,121 @@
+from multiprocessing import Pool
+import numpy as np
+from simulation import *
+import matplotlib.pyplot as plt
+import json
+
+N = 5 # size of object
+M = 100 # number of samples
+method = 2
+dyck_path_objs =[]
+dyck_paths = [""] * M
+binary_trees = [""] * M
+planar_trees =[""] * M
+noncrossings = [""] * M
+planar_tree_graphs = [""] * M
+binary_tree_graphs = [""] * M
+distribution = {}
+times = {"dyck":0.,"noncrossing":0.,"plane_tree":0., "plane_tree_graph":0., "binary_tree_graph":0.}
+#TODO generate M samples with each of 4 categories above in mathematica format
+#TODO save to file
+#TODO load and plot on mathematica
+def generate_mathematica_sample(i):
+    A = Catalan_Structure(N)
+    A.generate(method)
+    A.to_mathematica()
+    dyck_path_objs.append(A.dyck_path)
+    # print(f"\n\n ############################\n Generated sample {i}: \n")
+    # A.show()
+    # print(f"\n ############################\n\n")
+    
+    # A.check()
+    # A.show_times()
+    for time in times:
+        times[time] += A.times[time]
+    prefix = f"p{i} = "
+    dyck_path = (prefix + A.dyck_path_mathematica)
+    if A.dyck_path_mathematica in distribution:
+            distribution[A.dyck_path_mathematica] += 1
+    else:
+        distribution[A.dyck_path_mathematica] = 1
+    # binary_tree = (prefix + A.binary_tree_mathematica)
+    planar_tree= (prefix + A.plane_tree_mathematica)
+    noncrossing = (prefix + A.non_crossing_partition_mathematica)
+    planar_tree_graph = (prefix + A.plane_tree_graph_mathematica)
+    binary_tree_graph = (prefix + A.binary_tree_graph_mathematica)
+    return (dyck_path, planar_tree, noncrossing,planar_tree_graph,binary_tree_graph)
+
+
+    
+if __name__ == "__main__":
+    pool = Pool(8)
+    # res = pool.map(generate_mathematica_sample, range(M))
+    res = [generate_mathematica_sample(i) for i in range(M)]
+        
+    # pool.close()
+    # pool.join()
+    
+    for i in range(M):
+        dyck_paths[i] = res[i][0]
+        # binary_trees[i] = res[i][1]
+        planar_trees[i] = res[i][1]
+        noncrossings[i] = res[i][2]
+        planar_tree_graphs[i] = res[i][3]
+        binary_tree_graphs[i] = res[i][4]
+        
+    dyck_paths_str = f"time: {times['dyck']}, average: {times['dyck']/M}\n"+ "\n".join(dyck_paths)
+    # binary_trees_str = "\n".join(binary_trees)
+    planar_trees_str = f"time: {times['plane_tree']}, average: {times['plane_tree']/M}\n"+ "\n".join(planar_trees)
+    noncrossing_str = f"time: {times['noncrossing']}, average: {times['noncrossing']/M}\n"+ "\n".join(noncrossings)
+    planar_tree_graphs_str = f"time: {times['plane_tree_graph']}, average: {times['plane_tree_graph']/M}\n"+ "\n".join(planar_tree_graphs)
+    binary_tree_graphs_str = f"time: {times['binary_tree_graph']}, average: {times['binary_tree_graph']/M}\n"+ "\n".join(binary_tree_graphs)
+    with open(f"dyck_paths_N{N}_M{M}.txt", "w") as f:
+        f.write(dyck_paths_str)
+    # with open(f"binary_trees_N{N}_M{M}.txt", "w") as f:
+    #     f.write(binary_trees_str)
+    with open(f"planar_trees_N{N}_M{M}.txt", "w") as f:
+        f.write(planar_trees_str)
+    with open(f"noncrossing_N{N}_M{M}.txt", "w") as f:
+        f.write(noncrossing_str)
+    with open(f"planar_tree_graphs_N{N}_M{M}.txt", "w") as f:
+        f.write(planar_tree_graphs_str)
+    with open(f"binary_tree_graphs_N{N}_M{M}.txt", "w") as f:
+        f.write(binary_tree_graphs_str)
+        
+    print(f"Distribution of Dyck paths: {distribution}")
+    fig = plt.figure()
+    ax1 = fig.add_subplot(3, 1, 1)
+    ax2 = fig.add_subplot(3, 1, 2)
+    fig.tight_layout(pad=3.0)
+    ax1.set_title(f"Distribution of Dyck paths for N={N} and M={M}")
+    ax1.set_ylim(0,max(distribution.values())+ M/(2*(len(distribution))))
+    ax1.plot(list(distribution.values()))
+    ax2.set_title(f"Dyck paths for N={N} and M={M}")
+    c = 4*np.log(M)**2
+    
+    for path in dyck_path_objs:
+        ax2.plot(path.ys, color = "green", alpha = 1/c)
+    
+    fig.savefig(f"distribution_and_dyck_paths_N{N}_M{M}.png")
+    
+    fig2 = plt.figure(figsize=(20,20))
+    ys = np.linspace(0,N+1,N+1)
+    xs = np.linspace(0,2*N+1,2*N+1)
+    zs = np.zeros((N+1,2*N+1))
+    # print(f"dimensions: zs: {zs.shape} xs: {xs.shape} ys: {ys.shape}")
+    for path in dyck_path_objs:
+        for i in range(len(path.ys[:-1])):
+            zs[path.ys[i]][i] += 1
+
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+    contour = plt.contourf(xs,ys, zs,extend="max")
+    plt.colorbar(contour)
+    fig2.savefig(f"contour_N{N}_M{M}.png")
+
+    np.save(f"point_distribution_N{N}_M{M}",zs)
+    
+    
+    
+    
--- simulation.py
+++ simulation.py
@@ -1,60 +1,129 @@
 """Author: Yi Yao Tan"""
-from codecs import namereplace_errors
 import numpy as np
-import copy
-
+from typing import List, Tuple, Dict, Union, Any
+import time
+import matplotlib.pyplot as plt
 #Notes I don't know if the copy is actually useful
 #-----------------------------------------------
+
 class Catalan_Structure:
     """Generic randomly generated combinatorial object counted by Catalan numbers of class size n"""
     def __init__(self,n: int):
         """n is the number of nodes in the planar tree"""
         self.n: int = n
-        self.dyck_path: list = self.generate_dyck_path() #has to be called first.
-        self.plane_tree: Tree = self.to_planetree()
-        self.binary_tree: Binary_Tree = self.to_binary_tree()
-        print(f"self.dyck_path {self.dyck_path} \nself.plane_tree: {self.plane_tree} \nself.binary_tree: {self.binary_tree}")
+        self.dyck_path: DyckPath = DyckPath([])
+        self.plane_tree: Tree = Tree(0, [])
+        self.binary_tree: Binary_Tree = Binary_Tree(0)
+        self.non_crossing_partition: Noncrossing= Noncrossing([])
+        self.heights: List[List[Tuple[int,int]]] = []
+        self.plane_tree_graph: TreeGraph = TreeGraph()
+        self.binary_tree_graph: TreeGraph = TreeGraph()
+        self.times: Dict[str, float] = {"dyck":0,"noncrossing":0,"plane_tree":0, "plane_tree_graph":0, "binary_tree_graph":0}
+        
 
+    def generate(self, method: int= 1):
+        """Actually generate the dyck path, binary tree, and plane tree"""
+        start = time.time()
+        self.dyck_path: DyckPath = self.generate_dyck_path(method) #has to be called first.
+        stop = time.time()
+        self.times["dyck"] = stop - start
+        
+        start = time.time()
+        self.heights, self.non_crossing_partition = self.to_heights_and_noncrossing()
+        stop = time.time()
+        self.times["noncrossing"] = stop - start
+        
+        start  = time.time()
+        self.plane_tree: Tree = self.to_planetree()
+        stop = time.time()
+        self.times["plane_tree"] = stop - start
+        
+        start  = time.time()
+        self.plane_tree_graph: TreeGraph = self.to_planetree_graph()
+        stop = time.time()
+        self.times["plane_tree_graph"] = stop - start
+        
+        # start  = time.time()
+        # self.binary_tree: Binary_Tree = self.to_binary_tree()
+        # stop = time.time()
+        # self.times["binary_tree"] = stop - start
+        
+        start  = time.time()
+        self.binary_tree_graph: TreeGraph = self.to_binary_tree_graph()
+        stop = time.time()
+        self.times["binary_tree_graph"] = stop - start
+        
+        
+        
+    def show_times(self):
+        print(f"Times\n {self.times}")
+        
+    def to_mathematica(self):
+        self.dyck_path_mathematica: str = self.dyck_path.to_mathematica()
+        self.non_crossing_partition_mathematica: str = self.non_crossing_partition.to_mathematica()
+        self.plane_tree_mathematica: str = self.plane_tree.to_mathematica()
+        # self.binary_tree_mathematica: str = self.binary_tree.to_mathematica()
+        self.plane_tree_graph_mathematica: str = self.plane_tree_graph.to_mathematica()
+        self.binary_tree_graph_mathematica: str = self.binary_tree_graph.to_mathematica()
+        
+    def show(self):
+        print(f"Structures Generated:\nself.dyck_path {self.dyck_path}\nself.heights: {self.heights}\nself.non_crosing_partition: {self.non_crossing_partition}\nself.plane_tree: {self.plane_tree}\nself.plane_tree_graph: {self.plane_tree_graph} \nself.binary_tree_graph: {self.binary_tree_graph}")
+        
+    def show_mathematica(self):
+        print(f"self.dyck_path_mathematica: {self.dyck_path_mathematica}\nself.non_crossing_partition_mathematica: {self.non_crossing_partition_mathematica}\nself.plane_tree_mathematica: {self.plane_tree_mathematica}\n \nself.plane_tree_graph_mathematica: {self.plane_tree_graph_mathematica} \nself.binary_tree_graph_mathematica: {self.binary_tree_graph_mathematica}")
+        
+
+        
     def random_permutation(self,n: int):
         return np.random.permutation(n)
+
+    def random_permutation2(self,n: int):
+        tab = [i for i in range(n)]
+        for i in range(1,n):
+            j = np.random.randint(0,i)
+            tab[i], tab[j] = tab[j], tab[i]
+        return np.array(tab)
     
-    def generate_dyck_path(self):
+    def generate_dyck_path(self, method: int = 1):
         """ generate a random path dyck path of length 2n + 1"""
-        sigma = self.random_permutation(2*self.n +1)
+        if method ==1:
+            sigma = self.random_permutation(2*self.n +1)
+        else:
+            sigma = self.random_permutation2(2*self.n +1)
         w = []
+        v = []
         u = []
         for i in range(2*self.n+1):
             if sigma[i] < self.n :
                 w.append(1)
             else:
                 w.append(0)
-        # print(f"generated w count 0: {w.count(0)} count 1: {w.count(1)}")
         curr_height, min, ind = 0, 0, 0
         for i in range(2*self.n + 1):
             if w[i] == 1:
                 curr_height += 1
             else:
-                curr_height = curr_height - 1
+                curr_height -= 1
                 if curr_height < min:
                     min = curr_height
                     ind = i
-                
-        print(f"w: {w} min: {min} ind: {ind}")
-        #everything after ind is positive walk since it is minimum
+       
         u = w[ind+1:] + w[:ind+1]
-        return u
+        out = DyckPath(u)
+        return out
 
-    def check(self, w: list):
+    def check(self):
         """w is a path of length 2n+1 that should have n+1 down steps and n up steps""" 
-        print(f"count 0: {w.count(0)} count 1: {w.count(1)}")
+        print(f"count 0: {self.dyck_path.path.count(0)} count 1: {self.dyck_path.path.count(1)}")
     
-    def to_heights(self):
-        """convert a path to a set of height classes of glued nodes using created path to represent the path"""
+    def to_heights_and_noncrossing(self):
+        """convert a path to a set of height classes of glued nodes using created path to represent the path, also generates non-crossing partition"""
         heights = []
         stack = []
         curr_height = 0
-        for i in range(len(self.dyck_path)-1): #ignore last one, just 2n in the beginning
-            if self.dyck_path[i] == 1:
+        noncrossing: List[Tuple[int,int]]  = []
+        for i in range(len(self.dyck_path.path)-1): #ignore last one, just 2n in the beginning
+            if self.dyck_path.path[i] == 1:
                 curr_height += 1
                 if curr_height > len(heights) :
                     heights.append([])
@@ -62,12 +131,31 @@ class Catalan_Structure:
             else:
                 curr_height -= 1
                 heights[curr_height].append((stack.pop(),i))
+                noncrossing.append(heights[curr_height][-1])
+                
 
-        return heights
+        noncrossing_out = Noncrossing(noncrossing)
+        return heights, noncrossing_out
     
+    def to_planetree_graph(self):
+        """converts a path to a plane tree in TreeGraph format"""
+        stack  = [0]
+        counter = 1
+        TG = TreeGraph()
+        for i in self.dyck_path.path:
+            # print(f"i: {i} stack: {stack} TreeGraph: {TG}")
+            if i == 1:
+                TG.add_edge(stack[-1],counter)
+                stack.append(counter)
+                counter += 1
+            if i == 0:
+                stack.pop()
+        return TG
+            
+
     def to_binary_tree(self):
         """convert a heights representation to a binary tree of size n, dfs left to right label order"""
-        heights = self.to_heights()
+        heights = self.heights
         root = Binary_Tree((-1,2*self.n))
         if heights == []:
             return root #empty tree
@@ -83,25 +171,26 @@ class Catalan_Structure:
         root = Binary_Tree(tup)
         
         if heights == []: #top right corner
-            root.set_left(Binary_Leaf(0))
-            root.set_right(Binary_Leaf(1))
+            root.set_left(Binary_Leaf("left"))
+            root.set_right(Binary_Leaf("right"))
             return root
 
         else:
             #cases for left child
             if len(heights) > 1:
                 new_heights = heights[1:]
-                for i in range(len(new_heights[0])): #find the left child:
-                    # 1) belonging to the parent within the tuple range
+                for i in range(len(new_heights[0])): 
+                    #find the left child which is an inner vertex of the tree.
+                    # 1) find in the next height level a child node belonging to the parent within the tuple range
                     # 2) has the lowest index
                     if new_heights[0][i][0] > tup[0] and new_heights[0][i][1] < tup[1]:
                         root.set_left(self.to_binary_tree_rec(new_heights, heights[1][i]))
                         break
-                #some cases without left child:
+                #some cases without left child, a leaf or outer vertext
                 if root.left == None:
-                    root.set_left(Binary_Leaf(0))
+                    root.set_left(Binary_Leaf("left"))
             else:
-                root.set_left(Binary_Leaf(0)) #no greater height
+                root.set_left(Binary_Leaf("left")) #no greater height
                 
             #cases for right child, if there are nodes to the right of the same height 
             if len(heights[0]) > 1:
@@ -112,11 +201,27 @@ class Catalan_Structure:
                 
 
             return root
+    def to_binary_tree_graph(self):
+        """converts a dyck path to a binary tree graph in TreeGraph format"""    
+        BTG = TreeGraph()
+        stack = [0]
+        counter = 1
+        for i in self.dyck_path.path[:-1]:  
+            # print(f"i: {i} stack: {stack}")
+            if i == 1:
+                BTG.add_edge(stack[-1],counter) #left edge
+                stack.append(counter)
+                counter += 1
+            if i == 0:
+                stack.pop()
+                BTG.add_edge(stack[-1],counter) #add right edge
+                stack[-1] = counter #replace top of stack with new highest right edge
+                counter +=1
+        return BTG
                 
-        
     def to_planetree(self):
         """convert a path to a planar tree by using a height class representation"""
-        heights = self.to_heights()
+        heights = self.heights
         root = Tree((-1,2*self.n)) #plant plane tree with root of the whole path
         children = []
         for i in range(len(heights[0])):
@@ -136,9 +241,13 @@ class Catalan_Structure:
         else:    
             children = []
             for i in range(len(heights[0])):
-                if heights[0][i][0] > tup[0] and heights[0][i][1] < tup[1]: #this is a child
+                if heights[0][i][0] > tup[0] and heights[0][i][1] < tup[1]: 
+                    #this is a child. Parent is (a,b) and child is (c,d) if a < c < d < b, 
+                    #thus generate children subtrees with next new height level, new parent is the child
                     children.append(self.to_planetree_rec((heights[1:]), (heights[0][i])))
+                    
             if len(children)> 0:
+                #set the children for the Tree
                 root = Tree(tup)
                 root.set_children(children)
                 return root
@@ -155,8 +264,25 @@ class Node:
             children is a list of subtrees"""
         self.value = value
         self.children = children
+
+class TreeGraph:
+    def __init__(self):
+        self.edge_list = []
+        self.num_nodes = 0
+        
+    def __str__(self):
+        return f"TreeGraph({self.edge_list})"
     
-    
+    def add_edge(self, edge1:int, edge2:int):
+        self.edge_list.append((edge1, edge2))
+        self.num_nodes +=1
+        
+    def to_mathematica(self):
+        edge_list_mathematica = []
+        for edge in self.edge_list:
+            edge_list_mathematica.append(f"{edge[0]} -> {edge[1]}")
+        nodes_list = ",".join([str(i) for i in range(self.num_nodes)])
+        return f"TreeGraph ["+ "{" + ",".join(edge_list_mathematica) + '}, GraphLayout -> \"LayeredDigraphEmbedding\"]'
         
 class Tree(Node):
     def __init__(self, value, children = []):
@@ -166,6 +292,11 @@ class Tree(Node):
         children_str = ",".join((str(child) for child in self.children))
         return f"Tree(%s, [%s])" % (self.value, children_str)
     
+    def to_mathematica(self):
+        children_str = ",".join([child.to_mathematica() for child in self.children])
+        # print(f"children_str: {children_str}")
+        return f"Tree[a, {{%s}}]" % (children_str)
+        
     def set_children(self, new_children: list):
         """set list of children trees"""
         self.children: list = new_children
@@ -178,8 +309,10 @@ class Leaf(Node):
         super().__init__(value)
         
     def __str__(self):
-
         return f"Leaf(%s)" % (str(self.value))
+    
+    def to_mathematica(self):
+        return f"a" # % (str(self.value))
 
 ##------------------Binary Tree Class------------------
 class Binary_Node:
@@ -190,8 +323,12 @@ class Binary_Node:
         self.value = value
         self.left = left
         self.right = right
+        
     def __str__(self):
         return f"Binary_Node(%s, %s, %s)" % (str(self.value), str(self.left), str(self.right))
+    
+    def to_mathematica(self):
+        return "Binary_Node[%s, %s, %s]" % (str(self.value), str(self.left), str(self.right))
     
 class Binary_Tree(Binary_Node):
     def __init__(self, value, left = None, right = None):
@@ -200,6 +337,18 @@ class Binary_Tree(Binary_Node):
     def __str__(self):
         return f"Binary_Tree(%s, %s, %s)" % (str(self.value), str(self.left), str(self.right))
     
+    def to_mathematica(self):
+        if self.left is not None:
+            left = self.left.to_mathematica()
+            children = ""
+            if self.right is not None: 
+                right = self.right.to_mathematica() 
+                children = f"{{{left}, {right}}}"
+            else: 
+                children = f"{{{left}}}"
+            return f"Tree[ , {children}]"
+        return f"Tree[ , {{}}]"
+        
     def set_left(self, left: Binary_Node):
         self.left = left
         
@@ -213,13 +362,60 @@ class Binary_Leaf(Binary_Node):
     def __str__(self):
         return f"Binary_Leaf(%s)" % (str(self.value))
     
+    def to_mathematica(self):
+        return f" " #% (str(self.value))
+class Noncrossing:
+    def __init__(self, edge_list: List[Tuple[int,int]] ):
+        self.edge_list = edge_list
+        self.num_nodes = len(edge_list)*2
+        
+    def __str__(self):
+        return f"Noncrossing({self.edge_list})"
+    
+    def to_mathematica(self):
+        edge_str = ", ".join([f"UndirectedEdge[{edge[0]}, {edge[1]}]" for edge in self.edge_list])
+        vertex_str = ", ".join([str(i) for i in range(self.num_nodes)])
+        return f"Graph[{{{vertex_str}}}, {{{edge_str}}}, GraphLayout -> {{\"CircularEmbedding\", \"OptimalOrder\" -> False}}, VertexLabels -> \"Name\"]"
 
-
-
+class DyckPath:
+    def __init__(self, path: List[int], ys: List[int] =[]) :
+        self.path = path
+        self.ys = self.generate_ys() if len(ys) == 0 else ys
+        
+    def generate_ys(self) -> List[int]:
+        counter =0
+        ys = [0]
+        for i in self.path:
+            if i == 1:
+                counter +=1
+                ys.append(counter)
+            else:
+                counter -=1
+                ys.append(counter)
+        return ys
+                
+    def __str__(self):
+        return f"DyckPath({self.path})"
+    
+    def to_mathematica(self):
+        return f"{{{','.join([str(i) for i in self.path])}}}"
+    
+    def plot(self):
+        plt.title("Dyck Path")
+        print(f"self.ys: {self.ys}")
+        plt.xlim(0,len(self.ys)-2)
+        plt.ylim(0,max(self.ys)+1)
+        plt.plot(self.ys)
+        plt.show()
+    
 if __name__ == "__main__":
-    n = 50
-    Catalan_Structure(n)
-    # print(f"tree child 0: {tree.children[0]}")
+    n = 5
+    A = Catalan_Structure(n)
+    A.generate(2)
+    # A.dyck_path.plot()
+    print(f"A.dyck_path.ys: {A.dyck_path.ys} length: {len(A.dyck_path.ys)}")
+    A.to_mathematica()
+    A.show_mathematica()
 
     
         
